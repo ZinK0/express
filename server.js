54767353df16a15faf5c690b0a1334878cc00c74
Zin KO <zinko.it@outlook.com>
2024-12-20 23:51:57 +0630

    Added watch flag for dev environment that will auto refresh the server if there are any changes happen

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.get("/", (req, res) => {
+  // you can send with the HTML tags also ans send method is generally use, there are other methods also
+  // res.send("<h1>Hello World!</h1>");
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
