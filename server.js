73136f2e9624701a8424eda442cdd2014aa2b42b
Zin KO <zinko.it@outlook.com>
2024-12-21 00:53:37 +0630

    using env and store PORT then use with that

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import fs from "fs";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "public"))); // this will serve the static files from the public folder
 
